55fd6dda75e15af94102bf0f826972a46c76f197
Chuanqi Xu <466018122@qq.com>
2023-03-13 14:15:50 -0400

    update main.py

--- qiskit_job_manager/backend/main.py
+++ qiskit_job_manager/backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
+import argparse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Welcome to Qiskit Job Manager!"}
+    return {"messasge": "Welcome to Qiskit Job Manager!"}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, help="Host IP", default="127.0.0.1")
+    parser.add_argument('--port', type=int, help="Host Port", default=8000)
+
+    args = parser.parse_args()
+
+    uvicorn.run("main:app", host=args.host, port=args.port, reload=True)
